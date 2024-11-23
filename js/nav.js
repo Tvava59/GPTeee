ba88a61e46e5023caac43feb7a8a105114dcaf56
Tvava59 <127375727+Tvava59@users.noreply.github.com>
2024-11-24 01:06:23 +0800

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,6 @@
+
+    document.getElementById('menuButton').addEventListener('click', function () {
+    const sideMenu = document.getElementById('sideMenu');
+    sideMenu.classList.toggle('open');
+});
+
